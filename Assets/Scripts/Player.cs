c9fb0226f30f50161df1a99323324b0a965c3e4f
Viper-Tol <tendonkakami@outlook.com>
2025-03-19 23:44:37 +0800

    添加新文件和修改的文件

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Rigidbody2D rb;
+    public float MoveSpeed;
+    private float XInput;
+  
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
-        
+       
+        XInput = Input.GetAxis("Horizontal");
+        //获得水平轴的输入值，并将其赋值给XInput变量。这将用于控制角色的左右移动。
+        //水平输入值在-1到1之间，表示向左或向右移动。
+        rb.velocity = new Vector2(MoveSpeed * XInput,rb.velocity.y);
+       if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(rb.velocity.x,4);
+        }
     }
 }
